001c7b6017469ab389bfd22f4bcfd7ceb2b73f0c
Nika <n.rihlicka@gmail.com>
2022-03-23 18:52:21 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// calculate 25% of any number provided
+        int number = 400;
+        double perc_calc = 0.25 * number;
+        System.out.println("25% of " + number+ "is: " + perc_calc);
+    }
+}
